0826f85e21d0f638a181ec847dcde997068bb688
jrmkim50 <jrmkim50@gmail.com>
2020-09-22 00:38:00 -0400

    some refactoring and ui fixes

--- index.js
+++ index.js
@@ -3,18 +3,22 @@ var experienceElement = document.getElementById("experience");
 var projectsElement = document.getElementById("projects");
 var teachingElement = document.getElementById("teaching");
 
+function scrollToAnimation(x, y) {
+    window.scrollTo(x, y);
+}
+
 document.getElementById("skills-btn").addEventListener("click", () => {
-    window.scrollTo(skillsElement.getBoundingClientRect().x, skillsElement.getBoundingClientRect().y)
+    scrollToAnimation(skillsElement.getBoundingClientRect().x, skillsElement.getBoundingClientRect().y)
 })
 
 document.getElementById("experience-btn").addEventListener("click", () => {
-    window.scrollTo(experienceElement.getBoundingClientRect().x, experienceElement.getBoundingClientRect().y)
+    scrollToAnimation(experienceElement.getBoundingClientRect().x, experienceElement.getBoundingClientRect().y)
 })
 
 document.getElementById("projects-btn").addEventListener("click", () => {
-    window.scrollTo(projectsElement.getBoundingClientRect().x, projectsElement.getBoundingClientRect().y)
+    scrollToAnimation(projectsElement.getBoundingClientRect().x, projectsElement.getBoundingClientRect().y)
 })
 
 document.getElementById("teaching-btn").addEventListener("click", () => {
-    window.scrollTo(teachingElement.getBoundingClientRect().x, teachingElement.getBoundingClientRect().y)
+    scrollToAnimation(teachingElement.getBoundingClientRect().x, teachingElement.getBoundingClientRect().y)
 })

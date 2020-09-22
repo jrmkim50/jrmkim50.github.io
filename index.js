387a33d8259ffc6a1b35f694c7abb7c47b21ce4f
jrmkim50 <jrmkim50@gmail.com>
2020-09-22 01:05:54 -0400

    scrolling

--- index.js
+++ index.js
@@ -3,22 +3,40 @@ var experienceElement = document.getElementById("experience");
 var projectsElement = document.getElementById("projects");
 var teachingElement = document.getElementById("teaching");
 
-function scrollToAnimation(x, y) {
-    window.scrollTo(x, y);
+var x;
+var y;
+var time;
+var stepSize;
+var start = null;
+
+function scrollToAnimation(xCoord, yCoord, animationTime = 100) {
+    
 }
 
 document.getElementById("skills-btn").addEventListener("click", () => {
-    scrollToAnimation(skillsElement.getBoundingClientRect().x, skillsElement.getBoundingClientRect().y)
+    // scrollToAnimation(skillsElement.getBoundingClientRect().x, skillsElement.getBoundingClientRect().y)
+    document.querySelector('#skills').scrollIntoView({
+        behavior: 'smooth'
+    });
 })
 
 document.getElementById("experience-btn").addEventListener("click", () => {
-    scrollToAnimation(experienceElement.getBoundingClientRect().x, experienceElement.getBoundingClientRect().y)
+    // scrollToAnimation(experienceElement.getBoundingClientRect().x, experienceElement.getBoundingClientRect().y)
+    document.querySelector('#experience').scrollIntoView({
+        behavior: 'smooth'
+    });
 })
 
 document.getElementById("projects-btn").addEventListener("click", () => {
-    scrollToAnimation(projectsElement.getBoundingClientRect().x, projectsElement.getBoundingClientRect().y)
+    // scrollToAnimation(projectsElement.getBoundingClientRect().x, projectsElement.getBoundingClientRect().y)
+    document.querySelector('#projects').scrollIntoView({
+        behavior: 'smooth'
+    });
 })
 
 document.getElementById("teaching-btn").addEventListener("click", () => {
-    scrollToAnimation(teachingElement.getBoundingClientRect().x, teachingElement.getBoundingClientRect().y)
+    // scrollToAnimation(teachingElement.getBoundingClientRect().x, teachingElement.getBoundingClientRect().y)
+    document.querySelector('#teaching').scrollIntoView({
+        behavior: 'smooth'
+    });
 })

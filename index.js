a243e88fdb316159bf8eaf4142bdd1417087e24d
jrmkim50 <jrmkim50@gmail.com>
2020-09-22 01:17:27 -0400

    got animations down

--- index.js
+++ index.js
@@ -3,40 +3,28 @@ var experienceElement = document.getElementById("experience");
 var projectsElement = document.getElementById("projects");
 var teachingElement = document.getElementById("teaching");
 
-var x;
-var y;
-var time;
-var stepSize;
-var start = null;
-
-function scrollToAnimation(xCoord, yCoord, animationTime = 100) {
-    
-}
+const scrollTime = 500;
 
 document.getElementById("skills-btn").addEventListener("click", () => {
-    // scrollToAnimation(skillsElement.getBoundingClientRect().x, skillsElement.getBoundingClientRect().y)
-    document.querySelector('#skills').scrollIntoView({
-        behavior: 'smooth'
-    });
+    $("body,html").animate({
+        scrollTop: $("#skills").offset().top
+    }, scrollTime)
 })
 
 document.getElementById("experience-btn").addEventListener("click", () => {
-    // scrollToAnimation(experienceElement.getBoundingClientRect().x, experienceElement.getBoundingClientRect().y)
-    document.querySelector('#experience').scrollIntoView({
-        behavior: 'smooth'
-    });
+    $("body,html").animate({
+        scrollTop: $("#experience").offset().top
+    }, scrollTime)
 })
 
 document.getElementById("projects-btn").addEventListener("click", () => {
-    // scrollToAnimation(projectsElement.getBoundingClientRect().x, projectsElement.getBoundingClientRect().y)
-    document.querySelector('#projects').scrollIntoView({
-        behavior: 'smooth'
-    });
+    $("body,html").animate({
+        scrollTop: $("#projects").offset().top
+    }, scrollTime)
 })
 
 document.getElementById("teaching-btn").addEventListener("click", () => {
-    // scrollToAnimation(teachingElement.getBoundingClientRect().x, teachingElement.getBoundingClientRect().y)
-    document.querySelector('#teaching').scrollIntoView({
-        behavior: 'smooth'
-    });
+    $("body,html").animate({
+        scrollTop: $("#teaching").offset().top
+    }, scrollTime)
 })
